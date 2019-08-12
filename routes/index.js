0104775105a0e813f9c9d063d581ab25ffeb4148
rollgotobed <35453778+rollgotobed@users.noreply.github.com>
2019-08-12 11:13:33 +0800

    做了小部分修改，更适合使用

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var mtool = new (require('../lib/tools.js'))(conf);// 消息操作工具
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Naiveboom - 比较安全', isZhao: conf.isZhao});
+  res.render('index', { title: '阅后即焚 - 比较安全', isZhao: conf.isZhao});
 });
 
 /**
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
  * @return {[type]}      [description]
  */
 router.get(conf.reg_guid, function(req, res) {
-	res.render('look', {title: '查看内容 - naiveBoom!', reg_guid: conf.reg_guid});
+	res.render('look', {title: '查看内容 - 阅后即焚!', reg_guid: conf.reg_guid});
 })
 
 module.exports = router;
